afdb3ff42cefd1a7fe4856dc9ac1a591e9c0c097
John Patrick Pepito <trojohn94@gmail.com>
2019-07-01 06:51:50 +0800

    Updates

---
+++ precache-manifest.71c1417a0a4d7d7b50a289c034729aae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc2e7b85c6fc9810aa1b502ca1394c1a",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "91789f13d9e7518a909c",
+    "url": "/robofriends/static/css/main.3c0048f6.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/robofriends/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "91789f13d9e7518a909c",
+    "url": "/robofriends/static/js/main.d43d1933.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "3eb2be46a75f69a75ac6142a350c062c",
+    "url": "/robofriends/static/media/Patchy Robots.3eb2be46.woff"
+  }
+]);
